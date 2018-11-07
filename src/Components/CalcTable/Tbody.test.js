e46ced114f45b0e9793e26d799880d9a95799cc9
und3rdg <und3rdg@gmail.com>
2018-11-07 21:14:43 +0000

    Fix tests. Pass fake data to components.

--- src/Components/CalcTable/Tbody.test.js
+++ src/Components/CalcTable/Tbody.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Tbody from './Tbody';
 
+const base = [
+  { name: 'Nicotine' , ml: 0, gr: 0, percent: 7  , type:''},
+  { name: 'PG'       , ml: 0, gr: 0, percent: 23 , type:''},
+  { name: 'VG'       , ml: 0, gr: 0, percent: 60 , type:''},
+]
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Tbody />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const table = document.createElement('table');
+  ReactDOM.render(<Tbody base={base}/>, table);
+  ReactDOM.unmountComponentAtNode(table);
 });
--- src/Components/CalcTable/Thead.test.js
+++ src/Components/CalcTable/Thead.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Thead from './Thead';
 
+const titles = [ 'Ingredient', 'Millilitre', 'Gram', 'Percent' ]
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Thead />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const table = document.createElement('table');
+  ReactDOM.render(<Thead titles={titles}/>, table);
+  ReactDOM.unmountComponentAtNode(table);
 });
